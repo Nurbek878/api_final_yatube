71b2571c9d6c4512dcb9f3944f0fd557f8d945b4
Нурбек Орозалиев <nurik878878@yandex.ru>
2023-03-30 22:45:47 +0500

    Create urls, views

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,4 +1,15 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
+
+from api.views import CommentViewSet, PostViewSet
+
+router = routers.DefaultRouter()
+router.register(r'posts', PostViewSet, basename='posts')
+router.register(r'posts/(?P<post_id>\d+)/comments',
+                CommentViewSet, basename='comments')
 
 urlpatterns = [
+    path('v1/', include(router.urls)),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
 ]
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,1 +1,33 @@
-# TODO:  Напишите свой вариант
+from django.contrib.auth import get_user_model
+from django.shortcuts import get_object_or_404
+from rest_framework import permissions, viewsets
+
+from api.serializers import (CommentSerializer, PostSerializer)
+from api.permissions import OnlyAuthorCanChange
+from posts.models import Post, User
+
+User = get_user_model()
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = (permissions.IsAuthenticated,
+                          OnlyAuthorCanChange)
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = (permissions.IsAuthenticated,
+                          OnlyAuthorCanChange)
+
+    def get_queryset(self):
+        post = get_object_or_404(Post, pk=self.kwargs.get('post_id'))
+        return post.comments.all()
+
+    def perform_create(self, serializer):
+        post = get_object_or_404(Post, pk=self.kwargs.get('post_id'))
+        serializer.save(author=self.request.user, post=post)
