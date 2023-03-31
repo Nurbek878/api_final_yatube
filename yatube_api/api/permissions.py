fa9a75dcc4c1ed19f6dd2a277b53397ed4dd96cc
Нурбек Орозалиев <nurik878878@yandex.ru>
2023-03-31 21:54:23 +0500

    Create permissions, add image

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -1,9 +1,12 @@
 from rest_framework import permissions
 
 
-class OnlyAuthorCanChange(permissions.BasePermission):
-    message = 'Изменение чужого контента запрещено!'
+class AuthorOrReadOnly(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        return (request.method in permissions.SAFE_METHODS
+                or request.user.is_authenticated
+                )
 
     def has_object_permission(self, request, view, obj):
-        return (request.method in permissions.SAFE_METHODS
-                or obj.author == request.user)
+        return obj.author == request.user
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import get_object_or_404
 from rest_framework import permissions, viewsets
 
 from api.serializers import (CommentSerializer, PostSerializer)
-from api.permissions import OnlyAuthorCanChange
+from api.permissions import AuthorOrReadOnly
 from posts.models import Post, User
 
 User = get_user_model()
@@ -12,8 +12,7 @@ User = get_user_model()
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = (permissions.IsAuthenticated,
-                          OnlyAuthorCanChange)
+    permission_classes = (AuthorOrReadOnly,)
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
@@ -22,7 +21,7 @@ class PostViewSet(viewsets.ModelViewSet):
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
     permission_classes = (permissions.IsAuthenticated,
-                          OnlyAuthorCanChange)
+                          AuthorOrReadOnly,)
 
     def get_queryset(self):
         post = get_object_or_404(Post, pk=self.kwargs.get('post_id'))
